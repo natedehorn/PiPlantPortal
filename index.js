1d9f24237b1b9a83126a440d3a1f328a109a669b
Nate DeHorn <natedehorn@gmail.com>
2018-03-30 13:57:30 -0400

    Standard index.js style

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
 
-server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+server.listen(PORT, () => console.log(`Listening on $ { PORT }`))
 
 app
-.use(express.static(path.join(__dirname, 'public')))
-.use(bodyParser.urlencoded({ extended: true }))
-.use(bodyParser.json())
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'ejs')
-.get('/', (req, res) => res.render('pages/index'))
-.post('/', (req, res) => {
-  io.emit('post', req.body)
-  res.send(`PiPlantPortal: POST Recieved`)
-});
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .post('/', (req, res) => {
+    io.emit('post', req.body)
+    res.send(`PiPlantPortal: POST Recieved`)
+  })
 
-io.on('connection', function(socket){
-  console.log('user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
+io.on('connection', (socket) => {
+  console.log('user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})

7a72584e193a69f60f3cedc220d05fa85366275b
Nate DeHorn <natedehorn@gmail.com>
2018-03-30 14:57:33 -0400

    Standard client.js style

--- public/js/client.js
+++ public/js/client.js
@@ -1,8 +1,10 @@
-var socket = io.connect();
+/* global io $ */
+
+var socket = io.connect()
 socket.on('post', (data) => {
-  $('#temperature td').html(data.temperature);
-  $('#humidity td').html(data.humidity);
-  $('#light td').html(data.light);
-  $('#moisture td').html(data.moisture);
+  $('#temperature td').html(data.temperature)
+  $('#humidity td').html(data.humidity)
+  $('#light td').html(data.light)
+  $('#moisture td').html(data.moisture)
   console.log(data)
 })
